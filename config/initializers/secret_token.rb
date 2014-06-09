b4848052ae8b8dfce636346ddb4af8dde6bc2f0c
aardvarkk <clarksonian@gmail.com>
2014-06-09 00:51:08 -0400

    Working at getting response, but needs cookie to operate properly.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+What2watch::Application.config.secret_key_base = 'ee5c0f16b8968ad524724134e85660bd395bbcc81e5859e50d5310635d9a6b0189dc6fdcbbc55bde46b21a8705bba5ee7c26674e9e71807cc8b864aeca4a25b2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+What2watch::Application.config.session_store :cookie_store, key: '_what2watch_session'
